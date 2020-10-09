2f9f035d00689dd10b9596d5251d44eecc33f59d
Aeopp <irmxj777@gmail.com>
2020-10-09 21:22:34 +0900

    맵 제외하고 리소스 전부 끝남

--- Project/MFCTool/math.cpp
+++ Project/MFCTool/math.cpp
@@ -5,8 +5,6 @@
 matrix math::GetCameraJoomMatrix(const float JoomScale, const vec3 & ScreenSize)
 {
 	
-	
-	
 	matrix MCameraRelativeTranslation, MJoomScale, MScreenReturn;
 	const float halfX = ScreenSize.x / 2.f;
 	const float halfY = ScreenSize.y / 2.f;
